1da1951cb25f52bd2d073a81b60ddf910aa7a99d
Polsa96 <psabadell@outlook.com>
2021-12-22 23:07:07 +0100

    primer commit

---
+++ Operadores.js
@@ -0,0 +1,29 @@
+//ENUNCIAT
+    // 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+    // 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+    // 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+    // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+    // teniendo dos variables y = 10 y z = 5.
+
+    // 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+    // teniendo dos variables y = 10 y z = 5.
+
+    console.log("-----------------------");
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let resultado=10*5;
+console.log(resultado);
+console.log("-----------------------");
+
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+resultado=10/2;
+console.log(resultado);
+console.log("-----------------------");
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+resultado=15/9;
+console.log(resultado);
+console.log("-----------------------");
